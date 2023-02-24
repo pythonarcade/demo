b44eb9e5d3aaa4aba19bb76e4e047fdc6c590b52
Paul V Craven <paul.craven@optimizely.com>
2023-02-24 14:48:29 -0600

    Update

--- spatial_hash/spatial_hash.py
+++ spatial_hash/spatial_hash.py
@@ -2,11 +2,11 @@ import random
 import arcade
 from base_view import BaseView
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-COIN_COUNT = 500
+COIN_COUNT = 5000
 RECT_WIDTH = 100
 RECT_HEIGHT = 100
+CHANGE_X = 10
+CHANGE_Y = 4
 
 
 class SpatialHashDemo(BaseView):
@@ -19,21 +19,21 @@ class SpatialHashDemo(BaseView):
         for i in range(COIN_COUNT):
             coin = arcade.SpriteCircle(7, arcade.color_from_hex_string("#E96479"))
             coin.position = (
-                random.randrange(SCREEN_WIDTH),
-                random.randrange(SCREEN_HEIGHT),
+                random.randrange(self.window.width),
+                random.randrange(self.window.height),
             )
             self.coin_list.append(coin)
         arcade.set_background_color(arcade.color_from_hex_string("#F5E9CF"))
-        self.change_x = 2
-        self.change_y = 1
+        self.change_x = CHANGE_X
+        self.change_y = CHANGE_Y
 
     def on_draw(self):
         self.clear()
         self.coin_list.draw()
 
         arcade.draw_lrtb_rectangle_outline(*self.rect, arcade.color_from_hex_string("#4D455D"), border_width=4)
-        for y in range(0, SCREEN_HEIGHT, self.coin_list.spatial_hash.cell_size):
-            for x in range(0, SCREEN_WIDTH, self.coin_list.spatial_hash.cell_size):
+        for y in range(0, self.window.height, self.coin_list.spatial_hash.cell_size):
+            for x in range(0, self.window.width, self.coin_list.spatial_hash.cell_size):
                 arcade.draw_rectangle_outline(
                     x + self.coin_list.spatial_hash.cell_size // 2,
                     y + self.coin_list.spatial_hash.cell_size // 2,
@@ -46,7 +46,17 @@ class SpatialHashDemo(BaseView):
 
     def on_update(self, delta_time):
         """ Movement and game logic """
-        """ Handle Mouse Motion """
+
+        self.total_time += delta_time
+        if self.total_time > self.time_on_screen:
+
+            if not self.window.view_list:
+                self.window.create_views()
+
+            new_view = self.window.view_list.pop(0)
+            self.window.show_view(new_view)
+
+        # Handle moving rect
         l, r, t, b = self.rect
         l += self.change_x
         r += self.change_x
